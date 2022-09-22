0c45a43cbf68ec20b82f7c6629184c85941f113a
Reza Ahmadi <32667304+RezaAmd@users.noreply.github.com>
2022-09-22 14:44:39 +0330

    ♻️ Initialize main page name and logo.

--- techonit-identity/pages/auth/signin.js
+++ techonit-identity/pages/auth/signin.js
@@ -1,9 +1,7 @@
 export default function Signin() {
     return (
         <>
-            <h1 class="text-3xl font-bold underline">
-                Signin page
-            </h1>
+        
         </>
     )
 }
