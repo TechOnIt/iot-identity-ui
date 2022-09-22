0fa930183e2ea14a83151e9cd0fbe3a46e06e7fe
Reza Ahmadi <32667304+RezaAmd@users.noreply.github.com>
2022-09-22 13:36:50 +0330

    🌱 Add signin page.

---
+++ techonit-identity/pages/auth/signin.js
@@ -0,0 +1,9 @@
+export default function Signin() {
+    return (
+        <>
+            <h1 class="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </>
+    )
+}
