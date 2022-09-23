c2924c1011c3f262a5ab2d0571306933b1941ed8
Reza Ahmadi <32667304+RezaAmd@users.noreply.github.com>
2022-09-23 12:01:44 +0330

    🌱 Dynamic title.

--- techonit-identity/pages/_app.js
+++ techonit-identity/pages/_app.js
@@ -4,16 +4,26 @@ import "../styles/libs/font-awesome/css/fontawesome.min.css";
 import AuthLayout from '../components/auth/layouts/layout'
 import MainLayout from '../components/main/layouts/layout'
 import ManageLayout from '../components/manage/layouts/layout'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   // set layout
-  const Layout = Component.layout === 'auth' ? AuthLayout : 
-  Component.layout === 'manage' ? ManageLayout : MainLayout;
+  const Layout = Component.layout === 'auth' ? AuthLayout :
+    Component.layout === 'manage' ? ManageLayout : MainLayout;
 
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>Technology On It {Component.title ? ' - ' + Component.title : ''}</title>
+        if(Component.description){
+          <meta name="description" content={Component.description} />
+        }
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
