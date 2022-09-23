3e852d7307cb7712cd915418803c1640de77f150
Reza Ahmadi <32667304+RezaAmd@users.noreply.github.com>
2022-09-23 12:51:56 +0330

    🌱 create new user page.

---
+++ techonit-identity/pages/manage/user/new.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function New() {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+New.layout = 'manage';
+New.title = 'Create a new user account';
