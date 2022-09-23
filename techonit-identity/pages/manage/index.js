8ef68bcf233515c240a6bf4e0449ddd481257171
Reza Ahmadi <32667304+RezaAmd@users.noreply.github.com>
2022-09-23 16:02:41 +0330

    🎉 Add sample buttons.

--- techonit-identity/pages/manage/index.js
+++ techonit-identity/pages/manage/index.js
@@ -1,9 +1,33 @@
-import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Index() {
     return (
         <>
-        
+            {/* Buttons */}
+            <div className='mb-4'>
+                <label className='text-2xl font-bold'>Buttons</label>
+            </div>
+            <div className='flex flex-wrap gap-3'>
+                <Link href='/manage/structure/new'>
+                    <a className='btn'>Regular</a>
+                </Link>
+                <Link href='/manage/structure/new'>
+                    <a className='btn btn-primary'>Primary</a>
+                </Link>
+                <Link href='/manage/structure/new'>
+                    <a className='btn btn-primary-outline'>Primary Outline</a>
+                </Link>
+                <Link href='/manage/structure/new'>
+                    <a className='btn btn-secondary'>Secondary</a>
+                </Link>
+                <Link href='/manage/structure/new'>
+                    <a className='btn btn-secondary-outline'>Secondary Outline</a>
+                </Link>
+                <Link href='/manage/structure/new'>
+                    <a className='btn btn-legendary'>Legendary</a>
+                </Link>
+            </div>
+            {/*  */}
         </>
     )
 }
