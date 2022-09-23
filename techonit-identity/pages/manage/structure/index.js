2d32912f0ad3dc60a5b897ede16770d029311713
Reza Ahmadi <32667304+RezaAmd@users.noreply.github.com>
2022-09-23 19:11:46 +0330

    ♻️ Remove example buttons.

--- techonit-identity/pages/manage/structure/index.js
+++ techonit-identity/pages/manage/structure/index.js
@@ -3,24 +3,9 @@ import Link from 'next/link'
 export default function Index() {
     return (
         <>
-        <Link href='/manage/structure/new'>
-            <a className='btn btn-primary'>Primary</a>
-        </Link>
-        <Link href='/manage/structure/new'>
-            <a className='btn btn-primary-outline ml-3'>Primary Outline</a>
-        </Link>
-        <Link href='/manage/structure/new'>
-            <a className='btn btn-secondary ml-3'>Secondary</a>
-        </Link>
-        <Link href='/manage/structure/new'>
-            <a className='btn btn-secondary-outline ml-3'>Secondary Outline</a>
-        </Link>
-        <Link href='/manage/structure/new'>
-            <a className='btn btn-legendary ml-3'>Legendary</a>
-        </Link>
-        <Link href='/manage/structure/new'>
-            <a className='btn btn-legendary-outline ml-3'>Legendary Outline</a>
-        </Link>
+            <Link href='/manage/structure/new'>
+                <a className='btn btn-primary'>New</a>
+            </Link>
         </>
     )
 }
