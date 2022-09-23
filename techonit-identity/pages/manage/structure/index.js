67010bebd24569149100eefa1a72f0ad565ef772
Reza Ahmadi <32667304+RezaAmd@users.noreply.github.com>
2022-09-23 12:51:38 +0330

    ♻️ Add structure button link.

--- techonit-identity/pages/manage/structure/index.js
+++ techonit-identity/pages/manage/structure/index.js
@@ -1,9 +1,11 @@
-import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Index() {
     return (
         <>
-        
+        <Link href='/manage/structure/new'>
+            <a>New</a>
+        </Link>
         </>
     )
 }
