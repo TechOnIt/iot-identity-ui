ccc0daf112923a3518b0745e33788dd7a8b044a1
Reza Ahmadi <32667304+RezaAmd@users.noreply.github.com>
2022-09-23 15:58:17 +0330

    ♻️ Refactor signin page.

--- techonit-identity/pages/auth/signin.js
+++ techonit-identity/pages/auth/signin.js
@@ -1,38 +1,48 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Signin() {
     return (
         <>
-            <div className="flex flex-col justify-center h-[100%] px-8 md:px-[20%] lg:px-[30%]">
+            <div className="flex flex-col justify-center h-[100%] px-9 md:px-[30%] lg:px-[35%]">
                 {/* Image And Text */}
-                <div className="text-center flex flex-col mb-12">
-                    <div className='grid mb-3'>
+                <div className="text-center flex flex-col mb-14">
+                    <div className='grid mb-5'>
                         <Image src="/assets/Sun rise-cuate.svg" alt="Nature view" width={100} height={200} />
                     </div>
                     <div>
                         <label className="text-primary-500 text-2xl font-bold">Welcome</label>
-                        <p>Enter your phone number to signin or signup!</p>
+                        <p>Enter username and password to signin!</p>
                     </div>
                 </div>
                 {/* Form And Button */}
                 <div>
-                    <div className="mb-9">
+                    <div className="mb-14">
                         <div className="mb-2">
                             <input
-                                className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                                className=' block w-full py-2 px-2.5 bg-gray-50 border border-gray-300 text-gray-900 rounded-md
+                                focus:ring-blue-500 focus:border-blue-500dark:bg-gray-700
+                                dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500
+                                dark:focus:border-blue-500 text-center'
                                 maxLength={11}
                                 type='text'
                                 placeholder='Phone Number' />
                         </div>
                         <div>
                             <input
-                                className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-                                maxLength={11}
-                                type='text'
+                                className=' block w-full py-2 px-2.5 bg-primary-50 border border-gray-300 text-gray-900 rounded-md
+                                focus:ring-blue-500 focus:border-blue-500dark:bg-gray-700
+                                dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500
+                                dark:focus:border-blue-500 text-center'
+                                maxLength={30}
+                                type='password'
                                 placeholder='Password' />
                         </div>
                     </div>
-                    <div className='text-center'>
+                    <div className='flex gap-3 justify-center'>
+                        <Link href='/'>
+                            <a className='btn'>Back</a>
+                        </Link>
                         <button className="btn btn-primary">Login</button>
                     </div>
                 </div>
