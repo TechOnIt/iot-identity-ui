3421e35243907e49f8ab3dbd93fad1f474f37a6e
Reza Ahmadi <32667304+RezaAmd@users.noreply.github.com>
2022-09-25 23:52:34 +0330

    ♻️ Cleanup

--- techonit-identity/pages/_app.js
+++ techonit-identity/pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import '../components/manage/layouts/vertical-layout.css'
-import "../styles/libs/font-awesome/css/fontawesome.min.css";
+import "../styles/libs/font-awesome/css/fontawesome.min.css"
 import AuthLayout from '../components/auth/layouts/layout'
 import MainLayout from '../components/main/layouts/layout'
 import ManageLayout from '../components/manage/layouts/layout'
